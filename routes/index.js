9c034f1943df997282c9b559f0df1e8ee1eb41bd
clayton wright <claytonwright@claytons-MacBook-Pro.local>
2022-09-12 17:01:00 -0400

    completed day 1 code along

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+// Module.exports is listing the variables in this file to send to other files
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Because our base path for users.js is "/users" and the route names concatenate, the final path for this route is going to be "localhost:3000/users/all"
+router.get("/all", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+// The final url for this route is going to be "/users/single"
+router.get("/single", (req, res) => {
+  res.json({
+    success: true,
+    user: "Single User",
+  });
 });
 
 module.exports = router;
